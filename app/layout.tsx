b0f0cf15dbad4bdd62f180ea54888cc75a8afc92
hieunmh <hieunm.pt@gmail.com>
2023-10-03 09:27:57 +0700

    intro page

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10 
           h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'
         />
