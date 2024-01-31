e74ddc4dc6605ca23e456e5365a48d740cf0c734
hieunmh <hieunm.pt@gmail.com>
2024-01-31 22:53:05 +0700

    connect mongo

--- demo/src/main/java/com/spring/demo/DemoApplication.java
+++ demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.spring.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.spring.demo.product.Product;
+import com.spring.demo.product.ProductRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner (ProductRepository repository) {
+		return args -> {
+		 var product = Product.builder().name("Iphone").description("Iphone 15 Pro max").build();
+		 repository.insert(product);
+		};
 	}
 
 }
---
+++ demo/src/main/java/com/spring/demo/product/Product.java
@@ -0,0 +1,25 @@
+package com.spring.demo.product;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Document
+@Data
+@AllArgsConstructor
+@Builder
+public class Product {
+
+  @Id
+  private String id;
+
+  private String name;
+
+  private String description;
+
+
+  
+}
